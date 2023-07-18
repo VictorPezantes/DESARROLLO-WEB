63a6b328b879348f80a285eb7b3f5e01cee80434
kevinvg22 <kevin.vasquez12@inacapmail.cl>
2023-07-17 23:21:32 -0400

    Eva_4

---
+++ Eva_4_Kevines/Eva_4/src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./componentes/Header";
+import MainBody from "./componentes/MainBody"
+import ProductSection from "./componentes/ProductSection"
+import ContactForm from "./componentes/ContactForm"
+import Footer from "./componentes/Footer"
+
+const App = ()=> {
+  return (
+    <>
+      <Header/>
+      <MainBody/>
+      <ProductSection/>
+      <ContactForm/>
+      <Footer/>
+      
+      
+    </>
+  );
+}
+
+export default App;
